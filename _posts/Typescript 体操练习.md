75816abdc1038daf480eecf819f55ac53b4ed0d2
lizhensheng <lizhensheng@actionsky.com>
2023-06-12 11:13:39 +0800

    [feature]: Support high light and update bolg

--- _posts/Typescript 体操练习.md
+++ _posts/Typescript 体操练习.md
@@ -631,3 +631,19 @@ type Get<T, K extends string> = K extends keyof T
     : never
   : never;
 ```
+
+## 15. String to Number
+
+```typescript
+type cases = [
+  Expect<Equal<ToNumber<'0'>, 0>>,
+  Expect<Equal<ToNumber<'5'>, 5>>,
+  Expect<Equal<ToNumber<'12'>, 12>>,
+  Expect<Equal<ToNumber<'27'>, 27>>,
+  Expect<Equal<ToNumber<'18@7_$%'>, never>>,
+]
+```
+
+```typescript
+type ToNumber<S extends string> = S extends `${infer N extends number}` ? N : never;
+```
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import { AppProps } from "next/app";
-import { useEffect } from "react";
-import generateMouseEffect from "mouse-animation-effect";
-import "mouse-animation-effect/dist/index.css";
-import "../styles/index.css";
+import { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import hljs from 'highlight.js';
+import generateMouseEffect from 'mouse-animation-effect';
+import 'mouse-animation-effect/dist/index.css';
+import '../styles/index.css';
+import 'highlight.js/styles/github.css';
 
 const { initMouseEffect, removeMouseEffect } = generateMouseEffect({});
 
@@ -13,6 +15,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     return () => {
       removeMouseEffect();
     };
+  }, []);
+
+  useEffect(() => {
+    document.querySelectorAll('pre code').forEach((el) => {
+      hljs.highlightElement(el as HTMLElement);
+    });
   }, []);
   return <Component {...pageProps} />;
 }
